15f17065189a02aa41865268c5c80b51d4457e9c
Alex Kuznecov <alexander.kuznecov16@gmail.com>
2024-08-15 11:59:57 +0300

    formula show

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -59,6 +59,11 @@ const selectInequality = inequalityIndex => {
 		btn.classList.remove('disabled');
 		errorText.style.color = 'green'; // not error color
 		errorText.innerHTML = 'Valid inequality'; // not error text
+
+		const formulaArea = document.querySelector('.formula');
+		if (inequalityIndex == 2) {
+			formulaArea.innerHTML = `a ln(bx + c) + d &lt; 0`;
+		}
 	} else {
 		btn.disabled = true;
 		btn.classList.add('disabled');
@@ -85,10 +90,60 @@ const check = (inequalityIndex, a, b, c, d) => {
 
 	if (a < -100 || a > 100 || b < -100 || b > 100 || c < -100 || c > 100 || d < -100 || d > 100) {
 		result = ['Please enter numbers less than 100 and bigger than -100.', false];
-	} else if ('') {
+	} else if (inequalityIndex) {
+		const solving = solve(inequalityIndex, a, b, c, d);
+
+		result = [solving[0], solving[1]];
 	}
 
 	return result;
+};
+
+const solve = (inequalityIndex, a, b, c, d) => {
+	// Logarithmic
+	if (inequalityIndex == 2) {
+		const exponent = -d / a; // Exponent (negative value for correct endline)
+		const exponentValue = Math.exp(exponent); // Exponent value
+		const root = (exponentValue - c) / b; // Root
+
+		// Inequality sign symbol check
+		const inequalitySign = a < 0 || b < 0 ? '>' : '<';
+
+		// Get interval by inequality sign
+		let interval;
+		if (inequalitySign === '>') {
+			interval = `(${root.toFixed(2)}, +∞)`;
+		} else {
+			interval = `(-∞, ${root.toFixed(2)})`;
+		}
+
+		// Console log (correct solution) - удалить потом эти логи!!!
+		console.log(`1) ${a} ln(${b}x ${c < 0 ? c : '+ ' + c}) ${d < 0 ? '- ' + Math.abs(d) : '+ ' + d} < 0`);
+		console.log(`2) ${a} ln(${b}x ${c < 0 ? c : '+ ' + c}) < ${-d} | /${a}`);
+		console.log(`3) ln(${b}x ${c < 0 ? c : '+ ' + c}) ${a < 0 ? '>' : '<'} ${-d / a}`);
+		console.log(`4) ${b}x ${c < 0 ? c : '+ ' + c} ${a < 0 ? '>' : '<'} e^${exponent}`);
+		console.log(`5) ${b}x ${inequalitySign} e^${exponent} ${c < 0 ? '+ ' + c : '- ' + c}`); /*
+
+	Как -d/a превратилось в степень экспонента?
+
+	Я использовал свойство натурального логарифма: если ln(a) = b, значит a = e^b
+	Пример: ln(7x + 3) < -2.5
+
+	Шаги:
+	1) используем: ln(a) = b, значит a = e^b
+	2) получаем: 7x + 3 < e^(-2.5)
+	3) Продолжаем решать неравенство.
+
+	Оно помогает нам устранить логарифм для решения неравенства.
+
+	Поэтому получилось => bx + c (< или >) e^(-d / a)
+
+	*/
+		console.log(`6) x ${inequalitySign} (e^${exponent} ${c < 0 ? '+ ' + c : '- ' + c}) / ${b}`);
+		console.log(`Root: ${interval}`);
+
+		return [`Root: ${interval}`, true]; // print result
+	}
 };
 
 const result = (text, isTrue) => {
