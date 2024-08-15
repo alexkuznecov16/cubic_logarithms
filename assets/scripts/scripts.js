1e032b2641418a40b794c893ef9f9e283f61a527
Alex Kuznecov <alexander.kuznecov16@gmail.com>
2024-08-15 11:32:24 +0300

    canvas adding, styles and script start

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -47,6 +47,57 @@ function updateTranslatePosition() {
 window.addEventListener('DOMContentLoaded', updateTranslatePosition); // on refresh
 window.addEventListener('resize', updateTranslatePosition); // on resize (width change)
 
-const solve = () => {
-	console.log('Solved!');
+// Main javascript code
+const errorText = document.querySelector('.error'); // error text
+
+// Select inequality by <select> in html
+const selectInequality = inequalityIndex => {
+	const btn = document.getElementById('solveBtn');
+
+	if (inequalityIndex > 0) {
+		btn.disabled = false;
+		btn.classList.remove('disabled');
+		errorText.style.color = 'green'; // not error color
+		errorText.innerHTML = 'Valid inequality'; // not error text
+	} else {
+		btn.disabled = true;
+		btn.classList.add('disabled');
+		errorText.style.color = 'red'; // error color
+		errorText.innerHTML = 'Error: invalid inequality'; // error text
+	}
+};
+
+// main function
+const main = () => {
+	const selectedInequality = parseInt(document.getElementById('test').value, 10); // get selected inequality index
+	const input1 = parseInt(document.getElementById('input1').value, 10);
+	const input2 = parseInt(document.getElementById('input2').value, 10);
+	const input3 = parseInt(document.getElementById('input3').value, 10);
+	const input4 = parseInt(document.getElementById('input4').value, 10);
+
+	const checking = check(selectedInequality, input1, input2, input3, input4);
+
+	result(checking[0], checking[1]);
+};
+
+const check = (inequalityIndex, a, b, c, d) => {
+	let result = ''; // initial result
+
+	if (a < -100 || a > 100 || b < -100 || b > 100 || c < -100 || c > 100 || d < -100 || d > 100) {
+		result = ['Please enter numbers less than 100 and bigger than -100.', false];
+	} else if ('') {
+	}
+
+	return result;
+};
+
+const result = (text, isTrue) => {
+	const textarea = document.getElementById('result-area');
+	textarea.value = text;
+
+	if (isTrue) {
+		textarea.style.color = 'green';
+	} else {
+		textarea.style.color = 'red';
+	}
 };
